7de5ca30bcfdf5776d9a3107d1a6da9f09141e8c
Fathurozak Buhari <rozakbuhari@gmail.com>
2018-12-09 09:40:05 +0700

    Add styled-components

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+
+const Button = styled.button`
+  outline: none;
+  border: 1px solid #eeeeee;
+  border-radius: 4px;
+  background: #eeeeee;
+  color: #333333;
+  padding: 8px 16px;
+  &:hover {
+    border: 1px solid #81c784;
+  }
+`;
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Universal application cool!.
+        <p>Universal application!</p>
+        <Button>Get started</Button>
       </div>
     );
   }
--- src/renderer.js
+++ src/renderer.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { ServerStyleSheet } from 'styled-components'
 import template from './index.ejs';
 import App from './client/App';
 
 export default () => {
-  const content = renderToString(<App />);
+  const sheet = new ServerStyleSheet();
+  const content = renderToString(sheet.collectStyles(<App />));
+  const styleTags = sheet.getStyleTags();
 
   return template({
     content,
-    title: 'Universal React!'
+    title: 'Universal React!',
+    headHtmlSnippet: styleTags
   });
 };
